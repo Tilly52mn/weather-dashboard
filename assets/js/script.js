07095b243d79b6abee1b27c20027ef4c8b233788
Adam Tilly <tillyjadam@gmail.com>
2021-09-26 11:19:59 -0500

    now pull lon and lat from api next to put that into new api call to get the weather data wanted

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,40 +3,50 @@ var cityInputEl = document.querySelector("#search-form")
 var formSubmitHandler = function (event) {
     // prevent page from refreshing
     event.preventDefault();
-  
+
     // get value from input element
     var cityname = document.getElementById("city-input").value;
     console.log(cityname)
     if (cityname) {
-      getCityWeather(cityname);
-      cityInputEl.value = "";
+        getCityWeather(cityname);
+        cityInputEl.value = "";
     } else {
-      alert("Please enter a City");
+        alert("Please enter a City");
     }
-  };
+};
 
-  var getCityWeather = function (city) {
+var getCityWeather = function (city) {
     // format the open weather api url
-    // var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e479c4c59730296618273b6939c40da3';
-   var apiUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=e479c4c59730296618273b6939c40da3'
+
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e479c4c59730296618273b6939c40da3'
     // make a get request to url
     fetch(apiUrl)
-      .then(function (response) {
-        // request was successful
-        if (response.ok) {
-          console.log(response);
-          response.json().then(function (data) {
-            console.log(data);
-            console.log(response.data.coords[0].lat)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    var lat = data.coord.lat
+                    var lon = data.coord.lon
+return lat lon
             // displayRepos(data, city);
           });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Open weather');
-      });
-  };
+                } else {
+                    alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Open weather');
+        });
+        var apiWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=e479c4c59730296618273b6939c40da3';
+        fetch(apiWeatherUrl)
+            .then(function (response) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                }
+            });
+};
 
-  cityInputEl.addEventListener("submit", formSubmitHandler);
+cityInputEl.addEventListener("submit", formSubmitHandler);
