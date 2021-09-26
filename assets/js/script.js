5ae4ab791a298e39ae002ea4b6c0fb2db9e2efab
Adam Tilly <tillyjadam@gmail.com>
2021-09-26 10:02:47 -0500

    added js to take form input and inial start to calling api

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var cityInputEl = document.querySelector("#search-form")
+
+var formSubmitHandler = function (event) {
+    // prevent page from refreshing
+    event.preventDefault();
+    alert('clicked')
+  
+    // get value from input element
+    var cityname = cityInputEl.value.trim();
+    console.log(cityname)
+    // if (cityname) {
+    //   getCityWeather(cityname);
+    //   cityInputEl.value = "";
+    // } else {
+    //   alert("Please enter a City");
+    // }
+  };
+
+  var getCityWeather = function (city) {
+    // format the open weather api url
+    // var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e479c4c59730296618273b6939c40da3';
+   var apiUrl ='api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&cnt=1&appid={API key}'
+    // make a get request to url
+    fetch(apiUrl)
+      .then(function (response) {
+        // request was successful
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            // console.log(data);
+            // displayRepos(data, city);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
+
+  cityInputEl.addEventListener("submit", formSubmitHandler);
