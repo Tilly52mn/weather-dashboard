e90809751123c03e2495715009575a443ff3af73
Adam Tilly <tillyjadam@gmail.com>
2021-09-28 19:15:06 -0500

    now data recived is displayed on the webpage including the date.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,43 @@ var lon = null
 var lat = null
 var savedCities = null
 var currentDate = moment().format('l')
-console.log(currentDate)
-// function date() {
-//   var dateEl=  document.getElementById('date');
-//   dateEl.textContent("currentDate")
-// }
-// date()
+var day1Temp = null
+var day2Temp = null
+var day3Temp = null
+var day4Temp = null
+var day5Temp = null
+var day1Wind = null
+var day2Wind = null
+var day3Wind = null
+var day4Wind = null
+var day5Wind = null
+var day1Humidity = null
+var day2Humidity = null
+var day3Humidity = null
+var day4Humidity = null
+var day5Humidity = null
+var day1Weather = null
+var day2Weather = null
+var day3Weather = null
+var day4Weather = null
+var day5Weather = null
+var currentTemp = null
+var currentHumidity = null
+var currentWind = null
+var currentWeather = null
+var currentUVI = null
+var dateEl=  document.getElementById('date');
+var fiveDayEl =document.getElementById('five-day-row')
+dateEl.textContent=currentDate
+// var day1date =currentDate.clone().add(1, 'day')
+console.log(day1date)
 var formSubmitHandler = function (event) {
     // prevent page from refreshing
     event.preventDefault();
 
     // get value from input element
     var cityname = document.getElementById("city-input").value;
+    document.getElementById('city').textContent=cityname;
     console.log(cityname)
     if (cityname) {
         getCityWeather(cityname);
@@ -50,32 +75,34 @@ var getCityWeather = function (city) {
                             console.log(response);
                             response.json().then(function (data) {
                                 console.log(data);
-                                var currentTemp = data.current.temp;
-                                var currentWind = data.current.wind_speed
-                                var currentWeather = data.current.weather[0].description
+                                 currentTemp = data.current.temp;
+                                 currentWind = data.current.wind_speed
+                                 currentWeather = data.current.weather[0].icon
+                                 currentUVI = data.current.uvi
+                                 currentHumidity = data.current.humidity
                                 console.log(currentWeather);
-                                console.log(currentTemp)
-                                var day1Temp = data.daily[0].temp.max
-                                var day2Temp = data.daily[1].temp.max
-                                var day3Temp = data.daily[2].temp.max
-                                var day4Temp = data.daily[3].temp.max
-                                var day5Temp = data.daily[4].temp.max
-                                var day1Wind = data.daily[0].wind_speed
-                                var day2Wind = data.daily[1].wind_speed
-                                var day3Wind = data.daily[2].wind_speed
-                                var day4Wind = data.daily[3].wind_speed
-                                var day5Wind = data.daily[4].wind_speed
-                                var day1Humidity = data.daily[0].humidity
-                                var day2Humidity = data.daily[1].humidity
-                                var day3Humidity = data.daily[2].humidity
-                                var day4Humidity = data.daily[3].humidity
-                                var day5Humidity = data.daily[4].humidity
-                                var day1Weather = data.daily[0].weather[0].description
-                                var day2Weather = data.daily[1].weather[0].description
-                                var day3Weather = data.daily[2].weather[0].description
-                                var day4Weather = data.daily[3].weather[0].description
-                                var day5Weather = data.daily[4].weather[0].description
-                                
+                                console.log(currentUVI)
+                                 day1Temp = data.daily[0].temp.max
+                                 day2Temp = data.daily[1].temp.max
+                                 day3Temp = data.daily[2].temp.max
+                                 day4Temp = data.daily[3].temp.max
+                                 day5Temp = data.daily[4].temp.max
+                                 day1Wind = data.daily[0].wind_speed
+                                 day2Wind = data.daily[1].wind_speed
+                                 day3Wind = data.daily[2].wind_speed
+                                 day4Wind = data.daily[3].wind_speed
+                                 day5Wind = data.daily[4].wind_speed
+                                 day1Humidity = data.daily[0].humidity
+                                 day2Humidity = data.daily[1].humidity
+                                 day3Humidity = data.daily[2].humidity
+                                 day4Humidity = data.daily[3].humidity
+                                 day5Humidity = data.daily[4].humidity
+                                 day1Weather = data.daily[0].weather[0].icon
+                                 day2Weather = data.daily[1].weather[0].icon
+                                 day3Weather = data.daily[2].weather[0].icon
+                                 day4Weather = data.daily[3].weather[0].icon
+                                 day5Weather = data.daily[4].weather[0].icon
+                                setWeather(currentHumidity);
                             })
                         });
                 });
@@ -88,6 +115,40 @@ var getCityWeather = function (city) {
         });
 
 };
+var setWeather = function (humidity) {
+    fiveDayEl.setAttribute('display', 'flex');
 
+    document.getElementById('current-temp').textContent=currentTemp;
+    document.getElementById('current-wind').textContent=currentWind;
+    document.getElementById('current-humidity').textContent=humidity;
+    document.getElementById('current-uv').textContent=currentUVI;
+    document.getElementById('current-weather').textContent=currentWeather
+
+    document.getElementById('day-1-weather').textContent=day1Weather
+    document.getElementById('day-1-temp').textContent=day1Temp
+    document.getElementById('day-1-wind').textContent=day1Wind
+    document.getElementById('day-1-humidity').textContent=day1Humidity
+
+    document.getElementById('day-2-weather').textContent=day2Weather
+    document.getElementById('day-2-temp').textContent=day2Temp
+    document.getElementById('day-2-wind').textContent=day2Wind
+    document.getElementById('day-2-humidity').textContent=day2Humidity
+
+    document.getElementById('day-3-weather').textContent=day3Weather
+    document.getElementById('day-3-temp').textContent=day3Temp
+    document.getElementById('day-3-wind').textContent=day3Wind
+    document.getElementById('day-3-humidity').textContent=day3Humidity
+
+    document.getElementById('day-4-weather').textContent=day4Weather
+    document.getElementById('day-4-temp').textContent=day4Temp
+    document.getElementById('day-4-wind').textContent=day4Wind
+    document.getElementById('day-4-humidity').textContent=day4Humidity
+
+    document.getElementById('day-5-weather').textContent=day5Weather
+    document.getElementById('day-5-temp').textContent=day5Temp
+    document.getElementById('day-5-wind').textContent=day5Wind
+    document.getElementById('day-5-humidity').textContent=day5Humidity
+
+}
 
 cityInputEl.addEventListener("submit", formSubmitHandler);
